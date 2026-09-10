743f1aa757de2c50159d19dd96d124c0eb052de4
LeuJiuChuan2603224 <2603224@sit.singaporetech.edu.sg>
2026-09-10 21:53:17 +0800

    Reject Negative Task 5

--- L2/auditor.py
+++ L2/auditor.py
@@ -1,16 +1,20 @@
 inventory = 0
 quantity = 0
 
-enter_quantity = 0
+enter_quantity = ""
 
-while enter_quantity != "quit":
+while True:
     enter_quantity = input("Enter the quantity of stock: ")
-    if enter_quantity == "quit":
+    if enter_quantity.isdigit() or (enter_quantity.startswith("-") and enter_quantity[1:].isdigit()):
+        enter_quantity = int(enter_quantity)
+        if enter_quantity < 0:
+             print("Invalid input. Please enter a positive number.")
+             
+        else:
+            print ("You entered:", enter_quantity)
+    elif enter_quantity.lower() == "quit":
             print("Thank You and Have A Nice Day!")
             break
-    elif enter_quantity.isdigit():
-        enter_quantity = int(enter_quantity)
-        print ("You entered:", enter_quantity)
     else:
         print("Invalid input. Please enter a number or 'quit' to exit.")
     
