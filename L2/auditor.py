0f834b9b08d1d611833022aff7ac67a796fe1843
LeuJiuChuan2603224 <2603224@sit.singaporetech.edu.sg>
2026-09-10 21:09:55 +0800

    accpet stock as integer task 3

--- L2/auditor.py
+++ L2/auditor.py
@@ -6,5 +6,7 @@ enter_quantity = 0
 while enter_quantity != "quit":
     enter_quantity = input("Enter the quantity of stock: ")
     if enter_quantity == "quit":
-        break
+            break
+    enter_quantity = int(enter_quantity)
+  
 
