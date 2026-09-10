3f4f9f3500e9e729e6da91666f29f3874041f5be
LeuJiuChuan2603224 <2603224@sit.singaporetech.edu.sg>
2026-09-10 21:16:17 +0800

    check invalid input like number 'ten' - Task 4

--- L2/auditor.py
+++ L2/auditor.py
@@ -6,7 +6,13 @@ enter_quantity = 0
 while enter_quantity != "quit":
     enter_quantity = input("Enter the quantity of stock: ")
     if enter_quantity == "quit":
+            print("Thank You and Have A Nice Day!")
             break
-    enter_quantity = int(enter_quantity)
+    elif enter_quantity.isdigit():
+        enter_quantity = int(enter_quantity)
+        print ("You entered:", enter_quantity)
+    else:
+        print("Invalid input. Please enter a number or 'quit' to exit.")
+    
   
 
