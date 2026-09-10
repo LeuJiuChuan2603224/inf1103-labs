928924f07580fb7c589133ff9d44045cc63027ea
LeuJiuChuan2603224 <2603224@sit.singaporetech.edu.sg>
2026-09-10 20:40:55 +0800

    add while loop task 2

---
+++ L2/auditor.py
@@ -0,0 +1,10 @@
+inventory = 0
+quantity = 0
+
+enter_quantity = 0
+
+while enter_quantity != "quit":
+    enter_quantity = input("Enter the quantity of stock: ")
+    if enter_quantity == "quit":
+        break
+
