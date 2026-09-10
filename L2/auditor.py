8f8e11470033db7bf9041f79635a42fb1e7de855
LeuJiuChuan2603224 <2603224@sit.singaporetech.edu.sg>
2026-09-10 22:25:45 +0800

    Print rejected and failed and total processed when quit - Task 8

--- L2/auditor.py
+++ L2/auditor.py
@@ -1,6 +1,6 @@
 inventory = 0
 quantity = 0
-
+fail_count = 0
 enter_quantity = ""
 
 while True:
@@ -8,6 +8,7 @@ while True:
     if enter_quantity.isdigit() or (enter_quantity.startswith("-") and enter_quantity[1:].isdigit()): # since .isdigit only checks for string we can only hardcode to check for negative symbols
         enter_quantity = int(enter_quantity)
         if enter_quantity < 0:
+             fail_count += 1
              print("Invalid input. Please enter a positive number.") 
         else:
             print ("You entered:", enter_quantity)
@@ -17,10 +18,13 @@ while True:
                 print("You have exceed 500 units limt:",quantity)
                 break
     elif enter_quantity.lower() == "quit":
+            print("Total rejected/failed inputs:", fail_count)
+            print("Total unit processed:", quantity)
             print("Thank You and Have A Nice Day!")
             break
 
     else:
+        fail_count += 1
         print("Invalid input. Please enter a number or 'quit' to exit.")
     
   
