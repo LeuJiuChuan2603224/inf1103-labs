e1b7147d5e52f08519a7d2bfe2d710ee2e9e6bc9
LeuJiuChuan2603224 <2603224@sit.singaporetech.edu.sg>
2026-09-17 22:14:26 +0800

    Task 1 add get_invalid_input()

--- L3/modular_auditor.py
+++ L3/modular_auditor.py
@@ -3,29 +3,35 @@ quantity = 0
 fail_count = 0
 enter_quantity = ""
 
-while True:
+def get_valid_input():
     enter_quantity = input("Enter the quantity of stock: ")
-    if enter_quantity.isdigit() or (enter_quantity.startswith("-") and enter_quantity[1:].isdigit()): # since .isdigit only checks for string we can only hardcode to check for negative symbols
-        enter_quantity = int(enter_quantity)
-        if enter_quantity < 0:
-             fail_count += 1
-             print("Invalid input. Please enter a positive number.") 
-        else:
+    if enter_quantity.lower() == "quit":
+        return "quit"
+    elif enter_quantity.isdigit() or (enter_quantity.startswith("-") and enter_quantity[1:].isdigit()):
+         enter_quantity = int(enter_quantity)
+         if enter_quantity < 0:
+            print("Invalid input. Please enter a positive number.")
+            return None
+         else:
             print ("You entered:", enter_quantity)
-            quantity += enter_quantity
-            print("Total quantity in stock:", quantity)
-            if quantity>=500:
-                print("You have exceed 500 units limt:",quantity)
-                break
-    elif enter_quantity.lower() == "quit":
-            print("Total rejected/failed inputs:", fail_count)
-            print("Total unit processed:", quantity)
-            print("Thank You and Have A Nice Day!")
-            break
-
+            return enter_quantity
     else:
+      print("Invalid input. Please enter a number or 'quit' to exit.")   
+      return None
+
+while True:
+    entered_quantity = get_valid_input()
+    if entered_quantity == "quit":
+        print("Thank You and Have A Nice Day!")
+        break
+    elif entered_quantity is None:
         fail_count += 1
-        print("Invalid input. Please enter a number or 'quit' to exit.")
+    else: 
+     
+        if quantity>=500:
+            print("You have exceed 500 units limt:",quantity)
+            break
+   
     
   
 
